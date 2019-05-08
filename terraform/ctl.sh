a061e758bba3d5b0d387544beab8c1f95e8d8b8b
Miguel Angel Garcia <miguel.garcia@gmail.com>
2019-05-07 21:36:55 -0300

    implemented help in ctl.sh

--- terraform/ctl.sh
+++ terraform/ctl.sh
@@ -5,5 +5,23 @@ set -e
 BASE=`realpath \`dirname $0\``
 CMD=$1
 
+if [ "$CMD" == "help" ] || [ "$CMD" == "--help" ] || [ "$CMD" == "" ]; then
+
+cat <<EOF
+Usage: $0 <command> [args].
+
+Available commands:
+deps           Install required dependencies, like terraform.
+up             Provision the cluster
+destroy        Destroy all cluster's resources
+stop           Stop the cluster without destroying its resources
+restart        Restart a previously "stopped" cluster
+
+EOF
+
+exit 0
+
+fi
+
 shift
 cd $BASE ; ./bin/cmd/$CMD.sh $@
