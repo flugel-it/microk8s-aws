581cd5395481f096b48cc82a32a3bad563993e07
Miguel Angel Garcia <miguel.garcia@gmail.com>
2019-05-10 19:45:53 -0300

    fix to avoid forcing recreation

--- terraform-microk8s-aws/instance.tf
+++ terraform-microk8s-aws/instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "cluster" {
   ami             = "${data.aws_ami.cluster.id}"
   instance_type   = "${var.instance_type}"
-  security_groups = ["${aws_security_group.sg.id}"]
+  vpc_security_group_ids = ["${aws_security_group.sg.id}"]
   subnet_id       = "${aws_subnet.subnet-uno.id}"
   key_name = "${aws_key_pair.user-key.key_name}"
 
